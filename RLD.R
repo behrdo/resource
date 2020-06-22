8ac282f7f40b10b858c9350550c5ef35931b21ed
Naemi Kutzner <naemikutzner@Naemis-MBP.fritz.box>
2020-06-22 23:01:24 +0200

    w

--- RLD.R
+++ RLD.R
@@ -5,22 +5,21 @@ library(gridExtra)
 library(ggpubr)
 library(lme4)
 library(chillR)
-RLD <- read_delim("RLD3.csv", ";", escape_double = FALSE, 
-                       col_types = cols(`sampling date` = col_date(format = "%d.%m.%Y"),
-                                        `RLD total (cm cm-3)` = col_number(), 
-                                                `RLD bulk (cm cm-3)` = col_number(), 
-                                                  `RLD BP (cm cm-3)` = col_number(), 
-                                                  `RLU total (cm /m^2 in soil depth)` = col_number(), 
-                                                  `RLU bulk (cm /m^2 in soil depth)` = col_number(), 
-                                                 `RLU BP (cm /m^2 in soil depth)` = col_number()), 
-                     locale = locale(decimal_mark = ","), 
-                       trim_ws = TRUE)
+RLD <- read_delim("RLD.csv", ";", escape_double = FALSE, 
+                  col_types = cols(`sampling date` = col_date(format = "%d.%m.%Y"),
+                                   `RLD total (cm cm-3)` = col_number(), 
+                                   `RLD bulk (cm cm-3)` = col_number(), 
+                                   `RLD BP (cm cm-3)` = col_number(), 
+                                   `RL total (m m-2 in 5 cm soil depth)` = col_number(), 
+                                   `RL bulk (m m-2 in 5 cm soil depth)` = col_number(), 
+                                   `RL BP (m m-2 in 5 cm soil depth)` = col_number()), 
+                  locale = locale(decimal_mark = ","), 
+                  trim_ws = TRUE)
 
 RLD[17:33] <- NULL
 names(RLD)[5]<-"plot"
 names(RLD)[6]<-"field.rep"
 names(RLD)[8] <- "rainout.shelter"
-
 names(RLD)[10] <- "depth"
 names(RLD)[11] <- "tot_cm3"
 names(RLD)[12] <- "blk_cm3"
@@ -32,6 +31,7 @@ RLD[1] <- NULL
 RLD[2] <- NULL
 RLD[5] <- NULL
 RLD[4] <- NULL
+names(RLD)[1]  <- "sampling.date"
 
 Fe2 <- filter(RLD, treatment == "5")
 Ch2 <- filter(RLD, treatment == "6")
@@ -43,19 +43,24 @@ RLD <- RLD[complete.cases(RLD[ , 7:8]),]
 RLD$rainout.shelter[is.na(RLD$rainout.shelter)] = "rainfed"
 RLD$rainout.shelter[RLD$rainout.shelter == 'rainshelter'] <- 'rain shelter'
 
-RLD <- transform(RLD, treatment = as.factor(treatment),
-                 plot = as.factor(plot),
-                 rainout.shelter = as.factor(rainout.shelter))
-
 RLD <- separate(RLD, sampling.date, sep = "-", into =c("Year", "Month", "Day"))
 RLD <- make_JDay(RLD)
 RLD[2:3] <- NULL
 
-#summarize RLD of 5cm x m^2 to get total rootlength per m^2-------------------
+RLD <- transform(RLD, treatment = as.factor(treatment),
+                 plot = as.factor(plot),
+                 rainout.shelter = as.factor(rainout.shelter))
+RLD <- unite(RLD, Year, main.crop, col = Year, sep = "-")
+
+#Backup
+RLD1<- RLD
+RLD<- RLD1
+
+#Total Rootlegth (m m-2) Troughout the Year----------------------------------------
+#summarize RLD of 5cm x m^2 to get total rootlength per m^2
 
 RL_tot <- RLD %>% group_by(JDay, Year, treatment, rainout.shelter) %>% 
   summarise(tot_m2=sum(tot_m2), blk_m2=sum(blk_m2), BP_m2=sum(BP_m2))
-
 
 #plot
 ggplot(RL_tot, aes(x = JDay, y = blk_m2, colour = interaction(rainout.shelter, treatment),
@@ -63,8 +68,8 @@ ggplot(RL_tot, aes(x = JDay, y = blk_m2, colour = interaction(rainout.shelter, t
   geom_point() + geom_line() +
   facet_grid(cols = vars(Year)) +
   scale_colour_manual(values = c("brown4", "steelblue4", "brown2", "steelblue2"), name = "Treatment",
-                      labels = c("RS2 rain shelter", "RS2 rainfed", "WW2 rain shelter", "WW2 rainfed")) +
-  labs(x = "Day Number", y = "Rootlength [m  " ~m^2 ~"]", title = "Total Rootlegnth") +
+                      labels = c("Ch rain shelter", "Ch rainfed", "Fe rain shelter", "Fe rainfed"))+
+  labs(x = "Day Number", y = "Rootlength [m  " ~m^2 ~"]", title = "Total Rootlegth Troughout the Year") +
   theme_bw() +
   theme(axis.text = element_text(size = 10), 
         axis.title = element_text(size = 11), 
@@ -73,11 +78,11 @@ ggplot(RL_tot, aes(x = JDay, y = blk_m2, colour = interaction(rainout.shelter, t
         strip.text.x = element_text(size = 10)) + 
   theme_bw()
 # Data for 2016 is missing and some for 2017 too?
-
-#for max values compareing blk and BPfor all years and all depth-------------------------------
-
+-------------
+#for max values comparing blk and BP for all years and all depth-----not nice jet+Fehler--------------------------
+#FEHLER!!!
 RL_tot_max <- RL_tot %>%group_by(Year, treatment, rainout.shelter) %>%filter(JDay == max(JDay))
-RL_tot_max <- gather(RL_tot_max, "blk_m2", "BP_m2", key = "pore_kind", value = "m2")
+ ++++RL_tot_max <- gather(RL_tot_max, "blk_m2", "BP_m2", key = "pore_kind", value = "m2")
 RL_tot_max <- unite(RL_tot_max, treatment, rainout.shelter, col = treatment, sep = "-")
 #plot
 ggplot(RL_tot_max, aes(x = treatment, y = m2, fill = pore_kind)) +
@@ -86,23 +91,22 @@ ggplot(RL_tot_max, aes(x = treatment, y = m2, fill = pore_kind)) +
   theme_bw()
 
 
-#for single values of harvesting date------------------
-RLD1 <- gather(RLD,  "tot_cm3","BP_cm3", key = "pore_kind", value = "cm3")
-RLD1 <- RLD1 %>%group_by(pore_kind,Year, treatment, rainout.shelter, depth) %>%filter(JDay == max(JDay))
-RLD1 <- RLD1 %>% group_by(pore_kind, Year, treatment, rainout.shelter, depth) %>% 
+#for single values of harvesting date------------------------------------------------
+RLD2 <- gather(RLD,  "tot_cm3","BP_cm3", key = "pore_kind", value = "cm3")
+RLD2 <- RLD2 %>%group_by(pore_kind,Year, treatment, rainout.shelter, depth) %>%filter(JDay == max(JDay))
+RLD2 <- RLD2 %>% group_by(pore_kind, Year, treatment, rainout.shelter, depth) %>% 
   summarise(cm3=mean(cm3))
 
-#RLDY <- filter (RLD1, Year == 2017)
 #cm/ M!!!!!!!!!!!!!
 
-ggplot(RLD1, aes(x = depth, y = cm3, colour = interaction(rainout.shelter, pore_kind),
+ggplot(RLD2, aes(x = depth, y = cm3, colour = interaction(rainout.shelter, pore_kind),
                  group = interaction(pore_kind, rainout.shelter))) + 
    geom_line() +
   facet_grid(cols = vars(Year), rows = vars(treatment)) +
   scale_colour_manual(values = c( "olivedrab2", "green3","orange2", "orange4"), 
                       name = "Treatment",labels = c("BP Rain Shelter", "BP Rainfed",
                                                     "Total Rain Shelter", "Total Rainfed"))+
-  labs(x = bquote("Rootlength [m *" ~cm^-3 ~"]"), y = "Soil Depth [cm]", title = "Total Rootlegnth") +
+  labs(x = bquote("Soil Depth [cm]"), y= "Rootlength [m *" ~cm^-3 ~"]" , title = "Total Rootlegnth") +
   theme_bw() +
   scale_x_reverse()+
   coord_flip()+
