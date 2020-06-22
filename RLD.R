f42f8b2187518e4c63d7a29e8c665a2b3ef3d823
Naemi Kutzner <naemikutzner@Naemis-MBP.fritz.box>
2020-06-22 11:46:07 +0200

    1

--- RLD.R
+++ RLD.R
@@ -46,8 +46,9 @@ RLD <- transform(RLD, treatment = as.factor(treatment),
                  rainout.shelter = as.factor(rainout.shelter))
 
 RLD <- separate(RLD, sampling.date, sep = "-", into =c("Year", "Month", "Day"))
-
 RLD <- make_JDay(RLD)
+RLD[2:3] <- NULL
+
 
 #summarize RLD of 5cm x m^2 to get total rootlength per m^2
 RL_tot <- RLD %>% group_by(JDay, Year, treatment, rainout.shelter) %>% 
@@ -55,7 +56,7 @@ RL_tot <- RLD %>% group_by(JDay, Year, treatment, rainout.shelter) %>%
 
 
 #plot
-ggplot(RL_tot, aes(x = JDay, y = tot_m2, colour = interaction(rainout.shelter, treatment),
+ggplot(RL_tot, aes(x = JDay, y = blk_m2, colour = interaction(rainout.shelter, treatment),
                group = interaction(treatment, rainout.shelter))) + 
   geom_point() + geom_line() +
   facet_grid(cols = vars(Year)) +
@@ -70,11 +71,84 @@ ggplot(RL_tot, aes(x = JDay, y = tot_m2, colour = interaction(rainout.shelter, t
         strip.text.x = element_text(size = 10)) + 
   theme_bw()
 # Data for 2016 is missing and some for 2017 too?
-ww2 <- filter(RLD, treatment == "5")
 
+#for max values compareing blk and BPfor all years and all depth
+RL_tot_max <- RL_tot %>%group_by(Year, treatment, rainout.shelter) %>%filter(JDay == max(JDay))
+RL_tot_max <- gather(RL_tot_max, "blk_m2", "BP_m2", key = "pore_kind", value = "m2")
+RL_tot_max <- unite(RL_tot_max, treatment, rainout.shelter, col = treatment, sep = "-")
+#plot
+ggplot(RL_tot_max, aes(x = treatment, y = m2, fill = pore_kind)) +
+  geom_bar(stat = "identity") + 
+  facet_grid(cols = vars(Year)) +
+  theme_bw()
 
-# for different depth <30 />30
 
 #for single values
+RLD <- gather(RLD, "blk_m2", "BP_m2", key = "pore_kind", value = "m2")
+RLD <- RLD %>%group_by(pore_kind,Year, treatment, rainout.shelter, depth) %>%filter(JDay == max(JDay))
+RLD <- RLD %>% group_by(pore_kind, Year, treatment, rainout.shelter, depth) %>% 
+  summarise(m2=mean(m2))
+
+RLDY <- filter (RLD, Year == 2017)
 
 
+ggplot(RLDY, aes(x = depth, y = m2, colour = interaction(rainout.shelter, pore_kind),
+                 group = interaction(pore_kind, rainout.shelter))) + 
+   geom_line() +
+  facet_grid(cols = vars(treatment)) +
+  scale_colour_manual(values = c("brown4", "steelblue4", "brown2", "steelblue2"), name = "Treatment")+
+  #labels = c("RS2 rain shelter", "RS2 rainfed", "WW2 rain shelter", "WW2 rainfed")) +
+  #labs(x = "Day Number", y = "Rootlength [m  " ~m^2 ~"]", title = "Total Rootlegnth") +
+  theme_bw() +
+  scale_x_reverse(breaks=c(1,2,3,4))+
+  coord_flip()+
+  theme(axis.text = element_text(size = 10), 
+        axis.title = element_text(size = 11), 
+        plot.title = element_text(size = 15), 
+        strip.text.y = element_text(size = 10), 
+        strip.text.x = element_text(size = 10)) + 
+  theme_bw()
+
+
+scale_x_reverse(breaks=c(1,2,3,4), labels=c("0-30", "30-45", "45-75", "75-105"))
+  coord_flip()
+
+  # for different depth <30 / >30 at last sampling date
+
+  u30 <- filter(RLD, depth <=30)
+  o30 <- filter(RLD, depth >=30)
+  frames = c(u30, o30)
+  RL30 <- pd.concat (frames, keys =c("<30",">30") )
+  
+  u30 <- filter(RLD, depth <=30)
+  o30 <- filter(RLD, depth >=30)
+  # add colomn that distinguishes <30 / >30
+  
+  u30 <- u30 %>% group_by(JDay, Year, treatment, rainout.shelter) %>% 
+    summarise(tot_m2=sum(tot_m2), blk_m2=sum(blk_m2), BP_m2=sum(BP_m2))
+  o30 <- o30 %>% group_by(JDay, Year, treatment, rainout.shelter) %>% 
+    summarise(tot_m2=sum(tot_m2), blk_m2=sum(blk_m2), BP_m2=sum(BP_m2))
+  
+  
+  RL30 <- gather(RL30, "blk_m2", "BP_m2", key = "pore_kind", value = "m2")
+  
+  ggplot(RL30, aes(x = depth, y = m2, colour = interaction(rainout.shelter, treatment),
+                   group = interaction(treatment, rainout.shelter))) + 
+    geom_point() + geom_line() +
+    facet_grid(cols = vars(Year)) +
+    scale_colour_manual(values = c("brown4", "steelblue4", "brown2", "steelblue2"), name = "Treatment")+
+    #labels = c("RS2 rain shelter", "RS2 rainfed", "WW2 rain shelter", "WW2 rainfed")) +
+    #labs(x = "Day Number", y = "Rootlength [m  " ~m^2 ~"]", title = "Total Rootlegnth") +
+    theme_bw() +
+    scale_x_reverse(breaks=c(1,2,3,4))+
+    coord_flip()+
+    theme(axis.text = element_text(size = 10), 
+          axis.title = element_text(size = 11), 
+          plot.title = element_text(size = 15), 
+          strip.text.y = element_text(size = 10), 
+          strip.text.x = element_text(size = 10)) + 
+    theme_bw()
+  
+  
+  
+  
